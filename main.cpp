595407d7d2811d27955c5e99c6fb329faa7cdf69
Joseph Daniel Salas Rivas <j.salas.4@estudiantec.cr>
2024-11-07 13:33:50 -0600

    Fix: la ruta de los archivos se obtiene de manera dinamica

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@
 using namespace std;
 
 void loadInitialData(TravelGraph &graph, SimpleList<Client> &clients, SimpleList<Reward> &rewards) {
-	DB::loadDestinationsAndRoutes(R"(C:\Users\varga\OneDrive - Estudiantes ITCR\II Semestre\Estructuras de datos\Proyecto-2\data\destinations.json)", graph);
-	DB::loadClientsAndRewards(R"(C:\Users\varga\OneDrive - Estudiantes ITCR\II Semestre\Estructuras de datos\Proyecto-2\data\clients.json)", clients, rewards);
+	DB::loadDestinationsAndRoutes(R"(data\destinations.json)", graph);
+	DB::loadClientsAndRewards(R"(data\clients.json)", clients, rewards);
 
 	rewards.add(Reward("Boleto de avion", 1000));
 	rewards.add(Reward("Descuento en hotel", 500));
